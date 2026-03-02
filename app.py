4b39c0e5a2bf983e42354030dc5d45a91c0bee1a
anisa <anisa.dsouza@gmail.com>
2026-03-02 12:05:55 +0530

    updated files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1> anisa dsouza appid:2407188 </h1>"
+
+@app.route("/resume")
+def resume():
+    return render_templates("resume.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
